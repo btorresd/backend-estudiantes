c7342da2b2abd595013df37439feffad2295e89e
btorresd <be.torresd@duocuc.cl>
2025-09-22 16:29:10 -0300

    feat: implementacion delusuarioRepository con findByEmail

---
+++ src/main/java/com/backend/estudiantes/repository/UsuarioRepository.java
@@ -0,0 +1,12 @@
+package com.backend.estudiantes.repository;
+
+import com.backend.estudiantes.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findByEmail(String email);
+
+
+}
